47d4051059bcacd37fbfc86b32b491e113170e4b
Mahendra Singh Puniya <mahendrapuniya92@gmail.com>
2024-06-14 15:58:40 +0530

    this is repo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express')
 // import express from 'express'
 const app = express()
 
-const port = 4000
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
